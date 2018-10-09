c96e15e87dc9c61e2e8b22966955efb315dbedc6
MartinMatko <matias93@centrum.sk>
2018-10-09 21:15:50 +0200

    moving load to scorelib and fixing voice number
    moving load to scorelib and fixing voice number

--- 02-classes/scorelib.py
+++ 02-classes/scorelib.py
@@ -26,7 +26,7 @@ class Print:
         if self.edition.authors:
             print('Editor: ' + Person.get_author_string(self.edition.authors))
         for idx, voice in enumerate(self.edition.composition.voices):
-            print('Voice ' + str(idx) + ': ' + str(voice))
+            print('Voice ' + str(idx + 1) + ': ' + str(voice)) #voices start from one
         if self.partiture:
             print('Partiture: ' + 'yes')
         else:
@@ -60,7 +60,9 @@ class Voice:
     def __str__(self):
         result = ''
         if self.range:
-            result += self.range + ', '
+            result += self.range
+        if self.name and self.range:
+            result += ', '
         if self.name:
             result += self.name
         return result
@@ -104,6 +106,62 @@ class Person:
 
     @staticmethod
     def get_author_string(list):
-        return ('%s' % '; '.join(map(str, list)))
+        return '%s' % '; '.join(map(str, list))
 
+def load(file_name):
+    prints = []
+    with open(file_name, 'r', encoding="utf8") as file:
+        records = file.read().split('\n\n')
+    for record in records:
+        dict = {}
+        for line in record.split('\n'):
+            if re.match('^[A-Z]+', line) is not None:
+                key_value = line.split(':')
+                dict[key_value[0]] = key_value[1].strip()
+        prints.append(create_print_from_dictionary(dict))
+    return prints
+
+
+def create_print_from_dictionary(record):
+    print = Print()
+    print.print_id = int(record.get('Print Number'))
+    partiture = record.get('Partiture')
+    if 'es' in partiture:
+        print.partiture = True
+    else:
+        print.partiture = False
+
+    edition = Edition()
+    edition.name = record.get('Edition')
+    editors = record.get('Editor')
+    edition.authors = Person.get_author_list(editors)
+
+    composition = Composition()
+    composition.name = record.get('Title')
+    composition.incipit = record.get('Incipit')
+    composition.key = record.get('Key')
+    composition.genre = record.get('Genre')
+    if record.get('Year'):
+        composition.year = int(record.get('Year'))
+    composers = record.get('Composer')
+    composition.authors = Person.get_author_list(composers)
+
+    for i in range(1, 10):
+        voice_data = record.get('Voice ' + str(i))
+        if not voice_data:
+            break
+        else:
+            voice = Voice()
+            if '--' in voice_data:
+                range_and_name = voice_data.split(', ')
+                voice.range = range_and_name[0]
+                if len(range_and_name) > 1:
+                    voice.name = ', '.join(range_and_name[1:])
+            else:
+                voice.name = voice_data
+            composition.voices.append(voice)
+
+    edition.composition = composition
+    print.edition = edition
+    return print
 
--- 02-classes/test.py
+++ 02-classes/test.py
@@ -1,65 +1,6 @@
 import sys
 from scorelib import *
 
-def load(file_name):
-    prints = []
-    with open(file_name, 'r', encoding="utf8") as file:
-        records = file.read().split('\n\n')
-    for record in records:
-        dict = {}
-        for line in record.split('\n'):
-            if re.match('^[A-Z]+', line) is not None:
-                key_value = line.split(':')
-                dict[key_value[0]] = key_value[1].strip()
-        prints.append(create_print_from_dictionary(dict))
-    return prints
-
-
-def create_print_from_dictionary(record):
-    print = Print()
-    print.print_id = int(record.get('Print Number'))
-    partiture = record.get('Partiture')
-    if 'es' in partiture:
-        print.partiture = True
-    else:
-        print.partiture = False
-
-    edition = Edition()
-    edition.name = record.get('Edition')
-    editors = record.get('Editor')
-    edition.authors = Person.get_author_list(editors)
-
-    composition = Composition()
-    composition.name = record.get('Title')
-    composition.incipit = record.get('Incipit')
-    composition.key = record.get('Key')
-    composition.genre = record.get('Genre')
-    if record.get('Year'):
-        composition.year = int(record.get('Year'))
-    composers = record.get('Composer')
-    composition.authors = Person.get_author_list(composers)
-
-    for i in range(1, 10):#total hack
-        voice_data = record.get('Voice ' + str(i))
-        if not voice_data:
-            break
-        else:
-            voice = Voice()
-            if '--' in voice_data:
-                range_and_name = voice_data.split(', ')
-                voice.range = range_and_name[0]
-                if len(range_and_name) > 1:
-                    voice.name = range_and_name[1]
-            else:
-                voice.name = voice_data
-            composition.voices.append(voice)
-
-    edition.composition = composition
-    print.edition = edition
-    return print
-
-
-
 if not len(sys.argv) == 2:
     raise Exception("Invalid number of parameters")
 else:
